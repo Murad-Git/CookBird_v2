79d681c05a820eea1da15912bc67654c42598b9e
Murad Kos <Murad.ishmurodov@gmail.com>
2022-03-07 22:47:42 +0100

    changed/fixed scroll loadmore function, fixed diets and intolerances func in main page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,93 +1,113 @@
 'use strict'
 
 const backTopBtn = document.querySelector('.back-top');
-let offset = 0;
+
+const state = {
+    offset:6,
+    search:{
+        query:'',
+        results:[],
+        resultsPerPage:6,
+    }
+}
 
 // back to top btn
-// document.addEventListener('scroll', async function(e){
-//     let htmlEl
-//     const loadMoreDiv = document.querySelector('.js-load-more');
-//     const elBounding = loadMoreDiv.getBoundingClientRect();
-//     const parentEl = document.querySelector('.cookbird-thumbs-out')
-//     const scrollPosition = window.scrollY;
-//     if(scrollPosition > 300){
-//         backTopBtn.classList.add('back-top--active');
-//     }else{
-//         backTopBtn.classList.remove('back-top--active');
-//     }
+document.addEventListener('scroll', async function(e){
 
-//     // handling load more function
-//     if(elBounding.top>=0 && elBounding.left >=0 && elBounding.right <=window.innerWidth && elBounding.bottom <=window.innerHeight){
-//         async function  firstFunction() {
-//             // extract url search
-//             // const locationUrl = window.location.toString().split('?')[1];
-//             const location = new URL(window.location);
-//             const locationUrl = location.searchParams.get('search');
-//             if(locationUrl) {
-//                 htmlEl = await searchRecipes(locationUrl,offset);
-//                 parentEl.insertAdjacentHTML('beforeend', htmlEl);
-//                 offset+=10;
-//                 console.log(`offset: ${offset}`);
-//             }else{
-//                 htmlEl = await searchRecipes();
-//                 parentEl.insertAdjacentHTML('beforeend', htmlEl);
-//             }     
-//         }
-//         function secondaryFunction(){
-//              firstFunction(function() {
-//                 console.log(`I'm done`);
-//             });
-//         }
-//         secondaryFunction();
-//     }else{
-//         console.log(`Element is NOT in viewport!`);
-
-//     }
-// },true);
+    const scrollPosition = window.scrollY;
+    if(scrollPosition > 300){
+        backTopBtn.classList.add('back-top--active');
+    }else{
+        backTopBtn.classList.remove('back-top--active');
+    }
+}, true);
 
 backTopBtn.addEventListener('click', function(e){
     e.preventDefault();
     document.body.scrollIntoView({behavior:'smooth'});
 });
 
-// side menu collapse button
-// document.querySelector('.js-filters').addEventListener('click',function(e){
-//     const accordionInner = e.target.closest('.accordion-inner');
-//     if(!accordionInner) return;
-//     console.log(accordionInner);
-//     const arrowBtn = e.target.closest('.toggler');
-//     console.log(arrowBtn);
-    // const filterContent = filterBtn.nextElementSibling;
-    // if(accordionInner.classList.contains('show'))isOpen = true
-    // else isOpen = false;
+// handling load more function
+const scrollListener  = function(){
+    let htmlEl
+    const loadMoreDiv = document.querySelector('.js-load-more');
+    const elBounding = loadMoreDiv.getBoundingClientRect();
+    const parentEl = document.querySelector('.cookbird-thumbs-out');
 
-    // if(!isOpen){
+    if(elBounding.top>=0 && elBounding.left >=0 && elBounding.right <=window.innerWidth && elBounding.bottom <=window.innerHeight){
+        async function  firstFunction() {
+            // extract url search
+            // const locationUrl = window.location.toString().split('?')[1];
+            const location = new URL(window.location);
+            const getQuery = location.searchParams.get('search');
+            const getDiet = location.searchParams.get('diet');
+            const getIntolerances = location.searchParams.get('intolerances');
 
-    //     filterBtn.children[0].innerHTML = '<i class="fas fa-chevron-circle-down"></i>'
-    // }if(isOpen){
-    //     filterContent.classList.remove('dropdown-open');
-    //     filterContent.classList.add('dropdown-close');
-    //     filterBtn.children[0].innerHTML = '<i class="fa-solid fa-circle-chevron-up"></i>'
+            if(getQuery) {
+                htmlEl = await searchRecipes(`query=${getQuery}`,state.offset);
+                parentEl.insertAdjacentHTML('beforeend', htmlEl);
+                state.offset+=10;
+                // console.log(`offset: ${state.offset}`);
+            }else if (getDiet){
+                htmlEl = await searchRecipes(`diet=${getDiet}`,state.offset);
+                parentEl.insertAdjacentHTML('beforeend', htmlEl);
+                state.offset+=10;
+                // console.log(`offset: ${state.offset}`);
+            }
+            else if (getIntolerances){
+                htmlEl = await searchRecipes(`intolerances=${getIntolerances}`,state.offset);
+                parentEl.insertAdjacentHTML('beforeend', htmlEl);
+                state.offset+=10;
+                // console.log(`offset: ${state.offset}`);
+            }else{
+                htmlEl = await searchRecipes();
+                parentEl.insertAdjacentHTML('beforeend', htmlEl);
+            }     
+        }
+        function secondaryFunction(){
+            setTimeout(() => {
+                firstFunction(function() {
+                    console.log(`I'm done`);
+                });
+            }, 3000);
+        }
+        secondaryFunction();
+    }
+    // else{
+    //     console.log(`Element is NOT in viewport!`);
+
     // }
-// });
+};
 
-// 'see more' collapse button
-// document.querySelector('.js-filters').addEventListener('click', function(e) {
-//     const moreBtn = e.target.closest('.js-see-more-btn');
-//     if(!moreBtn) return;
-//     const ulElement = moreBtn.previousElementSibling;
-//     // console.log(ulElement);
+const throttledListener = throttle(scrollListener, 300);
+window.addEventListener('scroll', throttledListener);
 
-//     [...ulElement.children].map(li=>li.classList.contains('cousine-more')?li.classList.toggle('hide'):'');
+function throttle(func, delay) { // allows [func] to run once every [delay] ms
+    let funct = func.bind(func),
+        last = Date.now();
+    return function() {
+        if (Date.now() - last > delay) {
+            funct();
+            last = Date.now();
+        }
+    }
+}
 
-// });
-
-// search autocomplete 
-document.querySelector('.js-search-value').addEventListener('keyup', function(e){
+// search autocomplete
+document.querySelector('.js-search-value').addEventListener('keyup', async function(e){
+    const parentEl = document.querySelector('.js-autocomplete-parent');
     e.preventDefault();
-    // const data = await searchAutocomplete(this.value);
+    const htmlEl = await searchAutocomplete(this.value);
+    parentEl.innerHTML = ''
+    parentEl.insertAdjacentHTML('beforeend', htmlEl);
+});
+// close results list
+document.querySelector('.search-bar').addEventListener('click', function(e){
+    const searchResults = e.target.closest('.container').querySelector('.search-auto-item');
+    if(!searchResults) return;
+    searchResults.parentElement.innerHTML = ''
+});
 
-})
 // Media queries
 // collaps
 const mediaQuery = window.matchMedia("(max-width: 766px)");
@@ -107,41 +127,25 @@ handleTabletChange(mediaQuery);
 
 const searchRecipes = async function (query, offset){
     try {
-        let results;
-        let request;
-        if(query){
-            console.log(`I look for recipes by query: ${query}`);
-            request = await axios({
-                method: "GET",
-                url: `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=9&offset=${offset}&apiKey=c702c0d3e87142f2a528e780d52092a2`,
-            });
-            results = request.data.results.map(rec=>{
-                return {
-                    id : rec.id,
-                    image : `https://spoonacular.com/recipeImages/${rec.id}-636x393.jpg`,
-                    title : rec.title,
-                }
-            });
-        }
-        else{
-            console.log(`I look for random recipes`);
-            request = await axios({
-                method: "GET",
-                url: 'https://api.spoonacular.com/recipes/random?number=9&apiKey=c702c0d3e87142f2a528e780d52092a2',
-            });
-            results = request.data.recipes.map(rec=>{
-                return {
-                    id : rec.id,
-                    image : `https://spoonacular.com/recipeImages/${rec.id}-636x393.jpg`,
-                    title : rec.title,
-                }
-            });
-        }
+        const randomUrl = `https://api.spoonacular.com/recipes/random?number=${state.search.resultsPerPage}&apiKey=c702c0d3e87142f2a528e780d52092a2`
+        const queryUrl = `https://api.spoonacular.com/recipes/complexSearch?${query}&number=${state.search.resultsPerPage}&offset=${offset}&apiKey=c702c0d3e87142f2a528e780d52092a2`
+
+        const request = await axios({
+            method: 'GET',
+            url: query?queryUrl:randomUrl,
+        });
         // status check
         if(request.status == 200) console.log(`status: ${request.status}, data: ${request.data}`);
 
-        const html = results.map(result=>convertToHtml(result)).join('');
-        console.log(`offset in function: ${offset}`);
+        state.results = (query?request.data.results:request.data.recipes).map(rec=>{
+            return {
+                id : rec.id,
+                image : `https://spoonacular.com/recipeImages/${rec.id}-636x393.jpg`,
+                title : rec.title,
+            }
+        });
+        const html = state.results.map(result=>convertToHtml(result)).join('');
+        // console.log(`offset in function: ${offset}`);
         return html
     } catch (error) {
         console.error(`${error} 💥💥💥💥`);
@@ -168,7 +172,7 @@ const searchAutocomplete = async function(query){
     try {
         const request = await axios({
             method: 'GET',
-            url: `https://api.spoonacular.com/recipes/autocomplete?number=2&query=${query}&apiKey=c702c0d3e87142f2a528e780d52092a2`
+            url: `https://api.spoonacular.com/recipes/autocomplete?number=5&query=${query}&apiKey=c702c0d3e87142f2a528e780d52092a2`
         });
         const results = request.data.map(rec=>{
             return {
@@ -178,8 +182,23 @@ const searchAutocomplete = async function(query){
             }
         });
         console.log(results);
+        const html = results.map(result => searchHtml(result)).join('');
+        return html
     } catch (error) {
         console.error(`${error} 💥💥💥💥`);
         throw err;
     }
+}
+
+const searchHtml = function(recipe){
+    return `
+    <li class="search-auto-item">
+        <a class="search-element" href="/${recipe.id}">
+            <figure class="image">
+                <img src="${recipe.image}" alt="${recipe.title}">
+            </figure>
+            <p class="label">${recipe.title}</p>
+        </a>
+    </li>
+    `
 }
